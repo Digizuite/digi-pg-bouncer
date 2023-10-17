6be137fdd498007cb18d45d47d76e8e243106301
zlepper <rhdh@digizuite.com>
2023-10-17 14:01:16 +0200

    Log which event we got

--- run-pg-bouncer.sh
+++ run-pg-bouncer.sh
@@ -7,7 +7,8 @@ echo "Pg bouncer is PID: ${PG_BOUNCER_PID}"
 
 {
   inotifywait -r -m -e modify -e close_write -e move -e move_self -e delete /etc/pgbouncer |
-     while read -r _ _ _; do
+     while read -r file_path file_event file_name; do
+         echo ${file_path}${file_name} event: ${file_event}
          echo "Forcing pg bounder configuration reload..."
          kill -1 $PG_BOUNCER_PID
      done
