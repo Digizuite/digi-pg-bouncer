28a747fe8c5c3190adb6c42302820ac221976a97
zlepper <rhdh@digizuite.com>
2023-10-17 13:52:31 +0200

    Try another way 4

--- run-pg-bouncer.sh
+++ run-pg-bouncer.sh
@@ -6,7 +6,7 @@ PG_BOUNCER_PID=$!
 echo "Pg bouncer is PID: ${PG_BOUNCER_PID}"
 
 {
-  inotifywait -r -m -e modify /etc/pgbouncer |
+  inotifywait -r -m -e modify -e close_write -e move -e move_self -e delete /etc/pgbouncer |
      while read -r _ _ _; do
          echo "Forcing pg bounder configuration reload..."
          kill -1 $PG_BOUNCER_PID
